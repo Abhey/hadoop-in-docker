80e1eae92281599d962c0cd1509eef1164a8c26e
Abhey <abhey.mnnit@gmail.com>
2019-11-23 10:37:46 -0800

    Updated the image name to point to the one hosted on docker.io

--- deploy_hadoop_in_docker.sh
+++ deploy_hadoop_in_docker.sh
@@ -15,7 +15,7 @@ if [[ $? == 0 ]]; then
     echo 'Removing namenode instance.'
     docker rm hadoop-master
 fi
-    docker run -itd --network hadoop-net -p 9870:9870 -p 8088:8088 -p 19888:19888 --name hadoop-master -h hadoop-master hadoop-in-docker:latest
+    docker run -itd --network hadoop-net -p 9870:9870 -p 8088:8088 -p 19888:19888 --name hadoop-master -h hadoop-master uselesscoder/hadoop-in-docker:latest
 
 # Running container image of hadoop-in-docker for Hadoop-Slave-1.
 docker container ls | grep 'hadoop-slave-1' &> /dev/null
@@ -25,7 +25,7 @@ if [[ $? == 0 ]]; then
     echo 'Removing hadoop-slave-1 instance.'
     docker rm hadoop-slave-1
 fi
-docker run -itd --network hadoop-net --name hadoop-slave-1 -h hadoop-slave-1 hadoop-in-docker:latest
+docker run -itd --network hadoop-net --name hadoop-slave-1 -h hadoop-slave-1 uselesscoder/hadoop-in-docker:latest
 
 
 # Running container image of hadoop-in-docker for Hadoop-Slave-2.
@@ -36,7 +36,7 @@ if [[ $? == 0 ]]; then
     echo 'Removing hadoop-slave-2 instance.'
     docker rm hadoop-slave-2
 fi
-docker run -itd --network hadoop-net --name hadoop-slave-2 -h hadoop-slave-2 hadoop-in-docker:latest
+docker run -itd --network hadoop-net --name hadoop-slave-2 -h hadoop-slave-2 uselesscoder/hadoop-in-docker:latest
 
 # Formatting NameNode for first time run.
 docker exec hadoop-master bash -c "/hadoop/bin/hdfs namenode -format hadoop_cluster"
